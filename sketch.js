8fc8f7c58641928c6272b6f073a87a00646bda26
rkuper196 <81992046+rkuper196@users.noreply.github.com>
2021-05-19 23:34:12 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var s1,s2,s3,s4;
+var sprites =[]
+
+function setup() {
+  createCanvas(400, 400);
+  s1 = createSprite(75, 100, 30, 30);
+  s2 = createSprite(150, 250, 30, 30);
+  s3 = createSprite(300, 300, 30, 30);
+  s4 = createSprite(350, 150, 30, 30);
+  
+  sprites = [s1,s2,s3,s4]; 
+  
+ 
+}
+function draw() {
+
+  background(150,20,200);
+  drawSprites();
+  
+}
+
+
+
